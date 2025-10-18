bc207f9993ed5fa8b52ae20e093cf7a9e8fca565
FVS-MBA <feliven@gmail.com>
2025-10-18 16:46:38 -0300

    retira "assets" do endereço das imagens

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
+  imports: [Header, AppRoutingModule, Footer],
 })
 export class App {
   title = 'forms-reativos';
